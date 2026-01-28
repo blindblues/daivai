b9850251ce7822c5295e5e8bf9f6bca39bdd0810
blindblues <blindblues@users.noreply.github.com>
2026-01-28 21:38:39 +0100

    Configure Node.js adapter for server-side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blindblues.github.io',
   base: '/daivai',
   integrations: [react(), keystatic()],
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   build: {
     format: 'directory'
   }
