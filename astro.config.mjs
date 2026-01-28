ff69cf16b7e3dcf9e149fbbae3386c5880de169e
blindblues <blindblues@users.noreply.github.com>
2026-01-28 21:43:01 +0100

    Remove Keystatic integration to enable static build for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import keystatic from '@keystatic/astro';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blindblues.github.io',
   base: '/daivai',
-  integrations: [react(), keystatic()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  integrations: [react()],
+  output: 'static',
   build: {
     format: 'directory'
   }
